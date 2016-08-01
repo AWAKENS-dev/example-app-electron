1f97016ce81906a2f78df619987bd158d70b91d8
Kensuke Numakura <knmkr3gma@gmail.com>
2016-08-01 15:36:44 +0900

    switch pane by nav

--- renderer.js
+++ renderer.js
@@ -13,22 +13,39 @@ document.getElementById('select-file').addEventListener('click',function(){
     console.log(dialog.showOpenDialog({properties: ['openFile', 'openDirectory', 'multiSelections']}));
 });
 
-document.getElementById('get-genotype-btn').addEventListener('click',function(){
-    uri = path.join(addr, "/v1/genomes/1/genotypes");
-    request
-        .get(uri)
-        .query({locations: "chr19:45411941"})
-        .end(function(err, res){
-            prev = document.getElementById('get-genotype-res');
-            if (prev) {
-                prev.parentNode.removeChild(prev);
-            }
+// Switch pane by nav
+for (var nav of document.getElementsByClassName('nav-group-item')) {
+    nav.addEventListener('click',function(){
+	    var pane_id = this.getAttribute('data-pane');
 
-            var div = document.createElement('div');
-            div.className = 'well';
-            div.id = 'get-genotype-res';
-            div.textContent = res.text;
-            node = document.getElementById('get-genotype-code');
-            node.parentNode.insertBefore(div, node.nextSibling);
-        });
-});
+        for (var navItem of document.getElementsByClassName('nav-group-item')) {
+            navItem.classList.remove("active")
+        }
+        for (var paneContent of document.getElementsByClassName('pane-content')) {
+            paneContent.classList.remove("current")
+        }
+
+	    this.classList.add('active');
+	    document.getElementById(pane_id).classList.add('current');
+    })
+};
+
+// document.getElementById('get-genotype-btn').addEventListener('click',function(){
+//     uri = path.join(addr, "/v1/genomes/1/genotypes");
+//     request
+//         .get(uri)
+//         .query({locations: "chr19:45411941"})
+//         .end(function(err, res){
+//             prev = document.getElementById('get-genotype-res');
+//             if (prev) {
+//                 prev.parentNode.removeChild(prev);
+//             }
+
+//             var div = document.createElement('div');
+//             div.className = 'well';
+//             div.id = 'get-genotype-res';
+//             div.textContent = res.text;
+//             node = document.getElementById('get-genotype-code');
+//             node.parentNode.insertBefore(div, node.nextSibling);
+//         });
+// });
